e57fa14cedee294d048131f3932a8cad887955b0
Matthieu Archez (s0057016) <matthieu.archez@thalesgroup.com>
2018-12-18 15:12:34 +0000

    All tests OK - 10/10 pylint

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,18 @@
+"""
+	FIZBUZZ module
+"""
 
 def fizzbuzz(input_integer):
-	if input_integer % 15 == 0:
-		return "fizzbuzz"
-	elif input_integer % 3 == 0:
-		return "fizz"
-	elif input_integer % 5 == 0:
-		return "buzz"
-	else:
-		return input_integer
+    """
+        return fizz     when modulo 3
+               buzz     when modulo 5
+               fizzbuzz when modulo 15
+    """
+    if input_integer % 15 == 0:
+        return "fizzbuzz"
+    elif input_integer % 3 == 0:
+        return "fizz"
+    elif input_integer % 5 == 0:
+        return "buzz"
 
+    return input_integer
--- test.py
+++ test.py
@@ -1,44 +1,76 @@
+"""
+    Test module for fizzbuzz and tennis
+
+"""
+
 import unittest
-from fizzbuzz import fizzbuzz
-from tennis import inc_points
+from .fizzbuzz import fizzbuzz
+from .tennis import inc_points
 
-class Fizzbuzz_Test(unittest.TestCase):
-	def test_no_fizz_no_buzz(self):
-		self.assertEqual(fizzbuzz(1), 1)
-		self.assertEqual(fizzbuzz(2), 2)
-		self.assertEqual(fizzbuzz(98), 98)
+class FizzbuzzTest(unittest.TestCase):
+    """
+        Unittest implementation for fizzbuzz
+    """
+    def test_no_fizz_no_buzz(self):
+        """
+            Basic nominal tests
+        """
+        self.assertEqual(fizzbuzz(1), 1)
+        self.assertEqual(fizzbuzz(2), 2)
+        self.assertEqual(fizzbuzz(98), 98)
 
-	def test_fizz(self):
-		self.assertEqual(fizzbuzz(3), "fizz")
-		self.assertEqual(fizzbuzz(99), "fizz")
+    def test_fizz(self):
+        """
+            Fizz mod 3 tests
+        """
+        self.assertEqual(fizzbuzz(3), "fizz")
+        self.assertEqual(fizzbuzz(99), "fizz")
 
-	def test_buzz(self):
-		self.assertEqual(fizzbuzz(5), "buzz")
-		self.assertEqual(fizzbuzz(10), "buzz")
+    def test_buzz(self):
+        """
+            Buzz mod 5 tests
+        """
+        self.assertEqual(fizzbuzz(5), "buzz")
+        self.assertEqual(fizzbuzz(10), "buzz")
 
-	def test_fizzbuzz(self):
-		self.assertEqual(fizzbuzz(15), "fizzbuzz")
-		self.assertEqual(fizzbuzz(60), "fizzbuzz")
+    def test_fizzbuzz(self):
+        """
+            Fizzbuzz mod 15 tests
+        """
+        self.assertEqual(fizzbuzz(15), "fizzbuzz")
+        self.assertEqual(fizzbuzz(60), "fizzbuzz")
 
-class Tennis_Test(unittest.TestCase):
-	def test_inc_points_no_advantage(self):
-		self.assertEqual(inc_points(0, 0, 1), (15, 0))
-		self.assertEqual(inc_points(15, 0, 1), (30, 0))
-		self.assertEqual(inc_points(30, 30, 1), (40, 30))
-		self.assertEqual(inc_points(0, 30, 1), (15, 30))
+class TennisTest(unittest.TestCase):
+    """
+        Unittest implementation for Tennis
+    """
+    def test_inc_points_no_advantage(self):
+        """
+            Basic counting
+        """
+        self.assertEqual(inc_points(0, 0, 1), (15, 0))
+        self.assertEqual(inc_points(15, 0, 1), (30, 0))
+        self.assertEqual(inc_points(30, 30, 1), (40, 30))
+        self.assertEqual(inc_points(0, 30, 1), (15, 30))
 
-		self.assertEqual(inc_points(30, 15, 2), (30, 30))
-		self.assertEqual(inc_points(0, 30, 2), (0, 40))
+        self.assertEqual(inc_points(30, 15, 2), (30, 30))
+        self.assertEqual(inc_points(0, 30, 2), (0, 40))
 
-	def test_inc_points_advantages(self):
-		self.assertEqual(inc_points(40, 40, 2), (" ", "Ad."))
-		self.assertEqual(inc_points(40, 40, 1), ("Ad.", " "))
-		self.assertEqual(inc_points("Ad.", " ", 2), (40, 40))
-		self.assertEqual(inc_points(" ", "Ad.", 1), (40, 40))
+    def test_inc_points_advantages(self):
+        """
+            Advantages handling
+        """
+        self.assertEqual(inc_points(40, 40, 2), (" ", "Ad."))
+        self.assertEqual(inc_points(40, 40, 1), ("Ad.", " "))
+        self.assertEqual(inc_points("Ad.", " ", 2), (40, 40))
+        self.assertEqual(inc_points(" ", "Ad.", 1), (40, 40))
 
-	def test_inc_points_end_game(self):
-		self.assertEqual(inc_points(40, 30, 1), (0, 0))
-		self.assertEqual(inc_points(" ", "Ad.", 2), (0, 0))
+    def test_inc_points_end_game(self):
+        """
+            End game conditions
+        """
+        self.assertEqual(inc_points(40, 30, 1), (0, 0))
+        self.assertEqual(inc_points(" ", "Ad.", 2), (0, 0))
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
