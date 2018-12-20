fdc068ca117d11c1c840a34a9b10bcb265dfc99e
Matthieu Archez (s0057016) <matthieu.archez@thalesgroup.com>
2018-12-20 09:28:06 +0000

    All tests OK - tie break implemented

--- tennis.py
+++ tennis.py
@@ -1,8 +1,10 @@
 """
     Tennis module
 """
+NB_WINNING_SETS = 3
+
 class Score:
-    def __init__(self):
+    def __init__(self, nb_winning_sets):
         self.games  = [[0, 0]]
         self.points = [0, 0]
         self.tb_points = [0, 0]
@@ -15,29 +17,38 @@ def is_set_ending(game_winner, game_loser):
     """
     return True if ((game_winner == 6 and game_loser < 5) or (game_winner == 7 and game_loser in (5, 6))) else False
 
-def inc_game(game_p1, game_p2, winner_id):
+def inc_sets(sets_score, winner_id):
+    sets_p1, sets_p2 = sets_score[0], sets_score[1]
+    sets_winner, sets_loser = ((sets_p1 + 1), sets_p2) if winner_id == 1 else ((sets_p2 + 1), sets_p1)
+    return ((sets_winner, sets_loser), True) if sets_winner == NB_WINNING_SETS else ((sets_winner, sets_loser), False)
+
+
+def inc_game(game_score, winner_id):
     """
         Tennis board computing games
         return the updated point score, and True if the set is over, False otherwise
     """
+    game_p1, game_p2 = game_score[0], game_score[1]
     game_winner, game_loser = ((game_p1 + 1), game_p2) if winner_id == 1 else ((game_p2 + 1), game_p1)
     set_end = is_set_ending(game_winner, game_loser)
-    return (game_winner, game_loser, set_end) if winner_id == 1 else (game_loser, game_winner, set_end)
+    return ((game_winner, game_loser), set_end) if winner_id == 1 else ((game_loser, game_winner), set_end)
 
-def inc_tie_break(tb_p1, tb_p2, winner_id):
+def inc_tie_break(tb_score, winner_id):
     """
         Tennis tie break counter
     """
+    tb_p1, tb_p2 = tb_score[0], tb_score[1]
     tb_winner, tb_loser = ((tb_p1 + 1), tb_p2) if winner_id == 1 else ((tb_p2 + 1), tb_p1)
     tie_break_over = True if tb_winner >= 7 and tb_winner - tb_loser >= 2 else False
-    return (tb_winner, tb_loser, tie_break_over) if winner_id == 1 else (tb_loser, tb_winner, tie_break_over)
+    return ((tb_winner, tb_loser), tie_break_over) if winner_id == 1 else ((tb_loser, tb_winner), tie_break_over)
 
 
-def inc_points(pts_p1, pts_p2, winner_id):
+def inc_points(pts_score, winner_id):
     """
         Tennis board computing game points
         return the updated point score, and True if the point is over, False otherwise
     """
+    pts_p1, pts_p2 = pts_score[0], pts_score[1]
     pts_winner, pts_loser = (pts_p1, pts_p2) if winner_id == 1 else (pts_p2, pts_p1)
 
     game_end = False
@@ -64,4 +75,4 @@ def inc_points(pts_p1, pts_p2, winner_id):
         pts_winner = 40
         pts_loser = 40
 
-    return (pts_winner, pts_loser, game_end) if winner_id == 1 else (pts_loser, pts_winner, game_end)
+    return ((pts_winner, pts_loser), game_end) if winner_id == 1 else ((pts_loser, pts_winner), game_end)
--- test.py
+++ test.py
@@ -5,7 +5,7 @@
 
 import unittest
 from fizzbuzz import fizzbuzz
-from tennis import inc_points, inc_game, inc_tie_break, Score
+from tennis import inc_points, inc_game, inc_tie_break, inc_sets, Score
 
 class FizzbuzzTest(unittest.TestCase):
     """
@@ -48,86 +48,87 @@ class TennisTest(unittest.TestCase):
         """
             Basic counting
         """
-        self.assertEqual(inc_points(0, 0, 1), (15, 0, False))
-        self.assertEqual(inc_points(15, 0, 1), (30, 0, False))
-        self.assertEqual(inc_points(30, 30, 1), (40, 30, False))
-        self.assertEqual(inc_points(0, 30, 1), (15, 30, False))
+        self.assertEqual(inc_points([0, 0], 1), ((15, 0), False))
+        self.assertEqual(inc_points([15, 0], 1), ((30, 0), False))
+        self.assertEqual(inc_points([30, 30], 1), ((40, 30), False))
+        self.assertEqual(inc_points([0, 30], 1), ((15, 30), False))
 
-        self.assertEqual(inc_points(30, 15, 2), (30, 30, False))
-        self.assertEqual(inc_points(0, 30, 2), (0, 40, False))
+        self.assertEqual(inc_points([30, 15], 2), ((30, 30), False))
+        self.assertEqual(inc_points([0, 30], 2), ((0, 40), False))
 
     def test_inc_points_advantages(self):
         """
             Advantages handling
         """
-        self.assertEqual(inc_points(40, 40, 2), (" ", "Ad.", False))
-        self.assertEqual(inc_points(40, 40, 1), ("Ad.", " ", False))
-        self.assertEqual(inc_points("Ad.", " ", 2), (40, 40, False))
-        self.assertEqual(inc_points(" ", "Ad.", 1), (40, 40, False))
+        self.assertEqual(inc_points([40, 40], 2), ((" ", "Ad."), False))
+        self.assertEqual(inc_points([40, 40], 1), (("Ad.", " "), False))
+        self.assertEqual(inc_points(["Ad.", " "], 2), ((40, 40), False))
+        self.assertEqual(inc_points([" ", "Ad."], 1), ((40, 40), False))
 
     def test_inc_points_end_game(self):
         """
             End game conditions
         """
-        self.assertEqual(inc_points(40, 30, 1), (0, 0, True))
-        self.assertEqual(inc_points(" ", "Ad.", 2), (0, 0, True))
+        self.assertEqual(inc_points([40, 30], 1), ((0, 0), True))
+        self.assertEqual(inc_points([" ", "Ad."], 2), ((0, 0), True))
 
     def test_inc_game(self):
         """
             General case w/o tie break and set
         """
-        self.assertEqual(inc_game(0, 0, 1), (1, 0, False))
-        self.assertEqual(inc_game(0, 0, 2), (0, 1, False))
-        self.assertEqual(inc_game(1, 0, 1), (2, 0, False))
-        self.assertEqual(inc_game(4, 4, 1), (5, 4, False))
-        self.assertEqual(inc_game(5, 5, 2), (5, 6, False))
+        self.assertEqual(inc_game([0, 0], 1), ((1, 0), False))
+        self.assertEqual(inc_game([0, 0], 2), ((0, 1), False))
+        self.assertEqual(inc_game([1, 0], 1), ((2, 0), False))
+        self.assertEqual(inc_game([4, 4], 1), ((5, 4), False))
+        self.assertEqual(inc_game([5, 5], 2), ((5, 6), False))
 
     def test_inc_game_end(self):
         """
             Game ends
         """
-        self.assertEqual(inc_game(5, 4, 1), (6, 4, True))
-        self.assertEqual(inc_game(4, 5, 2), (4, 6, True))
-        self.assertEqual(inc_game(6, 5, 1), (7, 5, True))
+        self.assertEqual(inc_game([5, 4], 1), ((6, 4), True))
+        self.assertEqual(inc_game([4, 5], 2), ((4, 6), True))
+        self.assertEqual(inc_game([6, 5], 1), ((7, 5), True))
 
     def test_inc_game_tie_break(self):
-        self.assertEqual(inc_game(6, 5, 2), (6, 6, False))
-        self.assertEqual(inc_tie_break(5, 0, 1), (6, 0, False))
-        self.assertEqual(inc_tie_break(6, 0, 1), (7, 0, True))
-        self.assertEqual(inc_tie_break(6, 6, 1), (7, 6, False))
-        self.assertEqual(inc_tie_break(7, 6, 1), (8, 6, True))
-        self.assertEqual(inc_tie_break(17, 16, 2), (17, 17, False))
+        self.assertEqual(inc_game([6, 5], 2), ((6, 6), False))
+
+        self.assertEqual(inc_tie_break([5, 0], 1),   ((6, 0), False))
+        self.assertEqual(inc_tie_break([6, 0], 1),   ((7, 0), True))
+        self.assertEqual(inc_tie_break([6, 6], 1),   ((7, 6), False))
+        self.assertEqual(inc_tie_break([7, 6], 1),   ((8, 6), True))
+        self.assertEqual(inc_tie_break([17, 16], 2), ((17, 17), False))
 
     def test_game(self):
+        ###
         score = Score()
-        self.assertEqual(inc_points(score.points[0], score.points[1], 1), (15, 0, False))
+        self.assertEqual(inc_points(score.points, 1), ((15, 0), False))
 
+        ###
         score = Score()
         score.points = (40, 0)
-        self.assertEqual(inc_points(score.points[0], score.points[1], 1), (0, 0, True))
+        self.assertEqual(inc_points(score.points, 1), ((0, 0), True))
 
+        ###
         score = Score()
         score.points = (40, 0)
-        score.game = [(6, 0), (6, 0), (5, 6)]
+        score.games = [(6, 0), (6, 0), (6, 6)]
         score.sets = (2, 2)
+        score.tb_points = (6, 0)
         is_match_over = False
         id_winner = 1
 
         if score.games[-1] == (6, 6):
-        	score.tb_points, is_game_over = inc_tie_break(score.tb_points, id_winner)
+            score.tb_points, is_game_over = inc_tie_break(score.tb_points, id_winner)
         else:
-        	score.points, is_game_over = inc_points(score.points, id_winner)
+            score.points, is_game_over = inc_points(score.points, id_winner)
 
         if is_game_over:
             score.games[-1], set_is_over = inc_game(score.games[-1], id_winner)
             if set_is_over:
-                score.sets, match_is_over = inc_set(score.sets, id_winner)
-                assertEqual(match_is_over, True)
+                score.sets, is_match_over = inc_sets(score.sets, id_winner)
 
-            
-
-
-
+        self.assertEqual(is_match_over, True)
 
 if __name__ == "__main__":
     unittest.main()
